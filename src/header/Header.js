fae375661134126a0b995385651e0d4835e2d3ad
TeKronS <asper.tk93@gmail.com>
2022-07-14 19:10:23 -0400

    added animate header

--- src/header/Header.js
+++ src/header/Header.js
@@ -1,47 +1,221 @@
+import { useEffect, useRef } from "react";
 import {
   Header,
-  Name,
-  Title,
-  Text1,
-  Text2,
-  Text3,
-  Virgulilla,
-  BoxImgProfile,
-  BoxProfile,
+  ContentBox,
+  BoxColor,
+  Bala,
+  CirclePower,
+  Barr,
+  LinePower,
 } from "./styles";
 
-import ImgProfile from "./../img/Perfil/Y-O.jpg";
-
 export const HeaderSection = () => {
+  const refHeader = useRef(null);
+  const refBala = useRef(null);
+  const refLinePower = useRef(null);
+
+  useEffect(() => {
+    let clickPush = false;
+    let x = 82;
+    let xTouch = 82;
+    let y;
+    let element;
+    let selectElement;
+    let isTouch = false;
+    function triangleMove() {
+      const width = window.innerWidth;
+
+      // X condition
+      if (x > 50) {
+        let newX;
+        let LineX;
+
+        if (x > width - 75) {
+          newX = width - 100;
+          LineX = width - 70;
+        } else {
+          newX = x - 29;
+          LineX = x;
+        }
+        refHeader.current.style.clipPath = `polygon(
+          0px 85px,
+          3px 90px,
+          5px 93px,
+          10px 97px,
+          15px 99px,
+          20px 100px,
+          ${newX}px 100px,
+          ${newX + 30}px 130px,
+          ${newX + 60}px 100px,
+          calc(100% - 20px) 100px,
+          calc(100% - 15px) 99px,
+          calc(100% - 10px) 97px,
+          calc(100% - 5px) 93px,
+          calc(100% - 3px) 90px,
+          100% 85px,
+          100% 0px,
+          0px 0px
+        )`;
+        reziseLinePower(LineX);
+      } else {
+        reziseLinePower(50);
+
+        refHeader.current.style.clipPath = `polygon(
+          0px 85px,
+          3px 90px,
+          5px 93px,
+          10px 97px,
+          15px 99px,
+          20px 100px,
+          21px 100px,
+          51px 130px,
+          81px 100px,
+          calc(100% - 20px) 100px,
+          calc(100% - 15px) 99px,
+          calc(100% - 10px) 97px,
+          calc(100% - 5px) 93px,
+          calc(100% - 3px) 90px,
+          100% 85px,
+          100% 0px,
+          0px 0px
+        )`;
+      }
+    }
+
+    function reziseLinePower(end) {
+      const width = window.innerWidth;
+      const xline = end + 1;
+      const leftHeight = 10;
+      refBala.current.style.left = `${end - 2}px`;
+      refLinePower.current.style.backgroundImage = `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' version='1.1'%3E%3Cpath d='M ${xline} 78 C ${xline} ${
+        leftHeight + 20
+      } ${xline} ${leftHeight + 40} ${(width - xline) / 2 + xline - 30} ${
+        leftHeight + 30
+      } S ${width - 70} ${leftHeight + 30} ${
+        width - 70
+      } ${leftHeight} M 190 100 z'%0Astyle='fill:none;stroke:red;stroke-dasharray:5;stroke-linecap:round;stroke-width:3' /%3E%3C/svg%3E")`;
+    }
+
+    reziseLinePower(x);
+    function disparar() {
+      if (y && element) {
+        if (y > 130) {
+          clickPush = true;
+          selectElement = element;
+          refBala.current.style.transition = "350ms ease 0s";
+          refBala.current.style.top = `${y - 5}px`;
+          selectElement.style.transition = "175ms ease 175ms";
+          selectElement.style.boxShadow = " 0px 5px 15px 5px #ff4040";
+          if (isTouch) {
+            refBala.current.style.left = `${xTouch}px`;
+          }
+          setTimeout(() => {
+            selectElement.style.transition = "";
+            selectElement.style.boxShadow = "";
+            refBala.current.style.transition = "";
+            refBala.current.style.top = "120px";
+            if (isTouch) {
+              let LineX = x;
+              const width = window.innerWidth;
+
+              if (x > 50) {
+                if (x > width - 75) {
+                  LineX = width - 70;
+                } else {
+                  LineX = x;
+                }
+              } else {
+                LineX = 50;
+              }
+              refBala.current.style.left = `${LineX}px`;
+            }
+            setTimeout(() => {
+              if (clickPush) disparar();
+              else {
+                selectElement.click();
+              }
+            }, 20);
+          }, 350);
+        }
+      }
+    }
+
+    function mouseLeave() {
+      clickPush = false;
+    }
+
+    function touchst(e) {
+      isTouch = true;
+      if (e.touches[0]) {
+        x = e.touches[0].clientX;
+        xTouch = e.touches[0].clientX;
+        y = e.touches[0].clientY;
+        element = e.target;
+      } else {
+        x = 80;
+        y = 200;
+      }
+      triangleMove();
+      disparar();
+    }
+    function clickpush() {
+      if (!isTouch) {
+        disparar();
+      }
+    }
+    function touchmove(e) {
+      if (e.touches[0]) {
+        x = e.touches[0].clientX;
+        y = e.touches[0].clientY;
+        element = e.target;
+      } else {
+        x = 80;
+        y = 200;
+      }
+      triangleMove();
+    }
+    function clickmove(e) {
+      if (e.clientX && e.clientY) {
+        x = e.clientX;
+        y = e.clientY;
+        element = e.target;
+      } else {
+        x = 80;
+        y = 120;
+      }
+      triangleMove();
+    }
+
+    document.addEventListener("mousemove", clickmove);
+    document.addEventListener("mousedown", clickpush);
+    document.addEventListener("mouseup", mouseLeave);
+    document.addEventListener("touchmove", touchmove);
+    document.addEventListener("touchstart", touchst);
+    document.addEventListener("touchend", mouseLeave);
+    window.addEventListener("resize", triangleMove);
+
+    return () => {
+      document.removeEventListener("mousemove", clickmove);
+      document.removeEventListener("mousedown", clickpush);
+      document.removeEventListener("mouseup", mouseLeave);
+      document.removeEventListener("touchmove", touchmove);
+      document.removeEventListener("touchstart", touchst);
+      document.removeEventListener("touchend", mouseLeave);
+      window.removeEventListener("resize", triangleMove);
+    };
+  }, []);
   return (
     <Header>
-      <Name>Simón Ramírez </Name>
-
-      <BoxProfile>
-        <BoxImgProfile>
-          <img alt="" src={ImgProfile} width={200} />
-        </BoxImgProfile>
-        <Title>
-          <Text1>
-            Front-end
-            <Virgulilla>
-              <div>~~~~~~~~</div>
-            </Virgulilla>
-          </Text1>
-          <Text2>
-            Web
-            <Virgulilla>
-              <div>~~~~~~~~</div>
-            </Virgulilla>
-          </Text2>
-          <Text3>
-            Developer
-            <Virgulilla>
-              <div>~~~~~~~~</div>
-            </Virgulilla>
-          </Text3>
-        </Title>
-      </BoxProfile>
+      <ContentBox ref={refHeader}>
+        <BoxColor>
+          <LinePower ref={refLinePower} />
+          <Barr>
+            <span>Simón Ramírez</span>
+          </Barr>
+          <CirclePower />
+        </BoxColor>
+      </ContentBox>
+      <Bala ref={refBala} />
     </Header>
   );
 };
--- src/header/styles.js
+++ src/header/styles.js
@@ -1,280 +1,100 @@
 import styled from "styled-components";
 
-const DURATION = 15;
-const RESOLUTION_H = "1050px";
-const RESOLUTION_W = "620px";
-const RESOLUTION_F_H = "600px";
-const RESOLUTION_F_W = "380px";
-
 export const Header = styled.header`
-  display: grid;
-  min-height: 100vh;
-  overflow: hidden;
-  grid-gap: 20px;
-  grid-template-rows: 1fr 5fr;
-  @media screen and (min-width: ${RESOLUTION_H}) {
-    grid-template-rows: auto 1fr;
-  }
-  @media screen and (min-height: ${RESOLUTION_F_H}) and (max-width: ${RESOLUTION_F_W}) {
-    grid-template-rows: 1fr 7fr;
-  }
-
-  @keyframes animation1 {
-    0% {
-      transform: translate(-200%);
-    }
-    20% {
-      transform: translate(calc(100% * 5));
-    }
-    50% {
-      transform: translate(calc(100% * 5));
-    }
-    70% {
-      transform: translate(-200%);
-    }
-    100% {
-      transform: translate(-200%);
-    }
-  }
-  @keyframes animationBox {
-    0% {
-      margin-left: -30%;
-    }
-    50% {
-      margin-left: -13%;
-    }
-    100% {
-      margin-left: -30%;
-    }
-  }
-  @keyframes animation2 {
-    0% {
-      transform: translate(calc(100% * 5));
-    }
-    20% {
-      transform: translate(-200%);
-    }
-    50% {
-      transform: translate(-200%);
-    }
-    70% {
-      transform: translate(calc(100% * 5));
-    }
-    100% {
-      transform: translate(calc(100% * 5));
-    }
-  }
-  @keyframes textAnimated {
-    0%,
-    16.66%,
-    100% {
-      opacity: 0;
-    }
-    33.33% {
-      opacity: 1;
-    }
+  display: flex;
+  height: 130px;
+  position: sticky;
+  top: 0;
+  z-index: 55;
+  pointer-events: none;
+  div {
+    display: flex;
   }
 `;
 
-export const Title = styled.h1`
-  text-align: center;
-  font-size: 20vw;
-  font-family: "RobotoTK";
-  letter-spacing: -0.02em;
-  user-select: none;
-  box-sizing: border-box;
-  color: white;
-  margin: 0;
+export const BoxColor = styled.div`
+  position: absolute;
+  height: 130px;
   width: 100%;
-  max-width: 600px;
-  background-color: #1d1d1d;
-  background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='100' height='100'%3E%3Cdefs%3E%3ClinearGradient id='linear' x1='0%25' y1='0%25' x2='100%25' y2='0%25'%3E%3Cstop offset='0%25' stop-color='%23000000'/%3E%3Cstop offset='50%25' stop-color='%230A113A'/%3E%3Cstop offset='100%25' stop-color='%23260404'/%3E%3C/linearGradient%3E%3C/defs%3E%3Cpolygon fill='%231d1d1d' stroke='url(%23linear)' stroke-width='2px' points='0 0, 0 100, 50 50' /%3E%3Cpolygon fill='%231d1d1d' stroke='url(%23linear)' stroke-width='2px' points='100 0, 100 100, 50 50' /%3E%3Ccircle cx='0' cy='0' r='10' stroke='url(%23linear)' stroke-width='2px' fill='%231d1d1d' /%3E%3Ccircle cx='0' cy='100' r='10' stroke='url(%23linear)' stroke-width='2px' fill='%231d1d1d' /%3E%3Ccircle cx='100' cy='0' r='10' stroke='url(%23linear)' stroke-width='2px' fill='%231d1d1d' /%3E%3Ccircle cx='100' cy='100' r='10' stroke='url(%23linear)' stroke-width='2px' fill='%231d1d1d' /%3E%3Ccircle cx='50' cy='50' r='10' stroke='url(%23linear)' stroke-width='2px' fill='%231d1d1d' /%3E%3C/svg%3E");
-  background-attachment: fixed;
-
-  @media screen and (min-width: ${RESOLUTION_W}) {
-    font-size: 8rem;
-  }
-
-  @media screen and (min-width: ${RESOLUTION_H}) {
-    margin: auto -8px auto 0;
-    padding-bottom: 10px;
-    justify-self: end;
-    border-top: 2px solid white;
-    border-bottom: 2px solid white;
-    border-left: 2px solid white;
-    border-top-left-radius: 50px;
-    border-bottom-left-radius: 50px;
-    border-top-right-radius: 60px;
-    border-bottom-right-radius: 60px;
-    width: 105%;
-  }
-  span {
-    display: block;
-    position: relative;
-    line-height: 1.1;
-    padding-right: 10px;
-    margin-left: -5%;
-    white-space: nowrap;
-    width: 110%;
-    overflow: hidden;
-    :before {
-      display: block;
-      width: 100%;
-      opacity: 0;
-      position: absolute;
-      margin-left: -5px;
-      height: 170px;
-      left: 0;
-      right: 0;
-      inset: 0;
-      text-align: center;
-      white-space: nowrap;
-      -webkit-background-clip: text;
-      -webkit-text-fill-color: transparent;
-    }
-  }
+  top: 0;
+  left: 0;
+  background: linear-gradient(90deg, #007cf4, #00dfd8);
 `;
 
-export const Text1 = styled.span`
-  :before {
-    background: linear-gradient(90deg, #007cf4, #00dfd8);
-    animation: textAnimated ${DURATION / 2 + "s"} infinite 0s;
-    content: "Front-end";
-  }
-  div {
-    animation: animation1 ${DURATION + "s"} infinite 0s ease;
-    div {
-      background: linear-gradient(90deg, #007cf4, #00dfd8);
-      -webkit-background-clip: text;
-      -webkit-text-fill-color: transparent;
-    }
-  }
-`;
-export const Text2 = styled.span`
-  :before {
-    background: linear-gradient(90deg, #7928ca, #ff0080);
-    animation: textAnimated ${DURATION / 2 + "s"} infinite
-      ${DURATION / 3.3 / 2 + "s"};
-    content: "Web";
-  }
-  div {
-    animation: animation2 ${DURATION + "s"} infinite ${DURATION / 3.3 / 2 + "s"}
-      ease;
-    div {
-      background: linear-gradient(90deg, #7928ca, #ff0080);
-      -webkit-background-clip: text;
-      -webkit-text-fill-color: transparent;
-    }
-  }
-`;
-export const Text3 = styled.span`
-  :before {
-    background: linear-gradient(90deg, #ff4040, #f9cb28);
-    animation: textAnimated ${DURATION / 2 + "s"} infinite
-      ${DURATION / 3.3 + "s"};
-    content: "Developer";
-  }
-  div {
-    animation: animation1 ${DURATION + "s"} infinite ${DURATION / 3.3 + "s"}
-      ease;
-    div {
-      background: linear-gradient(90deg, #ff4040, #f9cb28);
-      -webkit-background-clip: text;
-      -webkit-text-fill-color: transparent;
-    }
-  }
-`;
-
-export const Virgulilla = styled.div`
-  position: absolute !important;
+export const ContentBox = styled.div`
+  position: relative;
+  height: 100%;
+  width: 100%;
+  border-bottom-left-radius: 30px;
+  border-bottom-right-radius: 30px;
   overflow: hidden;
-  width: 30%;
-  height: 22%;
-  inset: 0;
-  transform: translate(-100%);
-  top: 75%;
+
+  clip-path: polygon(
+    0px 85px,
+    3px 90px,
+    5px 93px,
+    10px 97px,
+    15px 99px,
+    20px 100px,
+    52px 100px,
+    82px 130px,
+    112px 100px,
+    calc(100% - 20px) 100px,
+    calc(100% - 15px) 99px,
+    calc(100% - 10px) 97px,
+    calc(100% - 5px) 93px,
+    calc(100% - 3px) 90px,
+    100% 85px,
+    100% 0px,
+    0px 0px
+  );
+`;
+
+export const Bala = styled.div`
+  width: 3px;
+  height: 10px;
+  position: absolute;
+  left: 80px;
+  top: 120px;
+  background-color: white;
+  border-radius: 3px;
+  border: 1px solid red;
+  z-index: 56;
+`;
+
+export const CirclePower = styled.div`
+  position: absolute;
+  height: 100px;
+  width: 100px;
   border-radius: 999px;
-  div {
-    top: -190%;
-    margin-left: -30%;
-    letter-spacing: -0.3em;
-    font-family: Arial, Helvetica, sans-serif;
-    position: absolute !important;
-    width: auto !important;
-    animation: animationBox 0.2s infinite linear;
+  top: -25px;
+  right: -20px;
+  background: radial-gradient(#ff4040 10%, #f9cb28);
+`;
+
+export const Barr = styled.div`
+  position: absolute;
+  height: 40px;
+  border-bottom: 1px solid #ff0080;
+  top: 0;
+  right: 0;
+  left: 0;
+  align-items: center;
+  padding-left: 20px;
+  font-size: 25px;
+  font-family: GothamTK, RobotoTK;
+  span {
+    background: linear-gradient(90deg, #1d1d1d, #ff4040);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
   }
 `;
 
-export const BoxProfile = styled.section`
-  display: grid;
-  grid-auto-flow: column;
-  grid-template-columns: 1fr auto;
-  border-top-left-radius: 999px;
-  border-bottom-left-radius: 999px;
-  border-top-right-radius: 70%;
-  border-bottom-right-radius: 70%;
-  background-color: #1d1d1d;
-  background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='100' height='100'%3E%3Cdefs%3E%3ClinearGradient id='linear' x1='0%25' y1='0%25' x2='100%25' y2='0%25'%3E%3Cstop offset='0%25' stop-color='%23f0f0f0'/%3E%3Cstop offset='50%25' stop-color='%23f0f0f0'/%3E%3Cstop offset='100%25' stop-color='%23260404'/%3E%3C/linearGradient%3E%3C/defs%3E%3Cpolygon fill='%231d1d1d' stroke='url(%23linear)' stroke-width='2px' points='0 0, 0 100, 50 50' /%3E%3Cpolygon fill='%231d1d1d' stroke='url(%23linear)' stroke-width='2px' points='100 0, 100 100, 50 50' /%3E%3Ccircle cx='0' cy='0' r='10' stroke='url(%23linear)' stroke-width='2px' fill='%231d1d1d' /%3E%3Ccircle cx='0' cy='100' r='10' stroke='url(%23linear)' stroke-width='2px' fill='%231d1d1d' /%3E%3Ccircle cx='100' cy='0' r='10' stroke='url(%23linear)' stroke-width='2px' fill='%231d1d1d' /%3E%3Ccircle cx='100' cy='100' r='10' stroke='url(%23linear)' stroke-width='2px' fill='%231d1d1d' /%3E%3Ccircle cx='50' cy='50' r='10' stroke='url(%23linear)' stroke-width='2px' fill='%231d1d1d' /%3E%3C/svg%3E");
-  background-attachment: fixed;
-  margin: auto;
-
-  @media screen and (min-width: ${RESOLUTION_H}) {
-    width: 90%;
-    max-width: 1000px;
-    height: 90%;
-    min-height: 500px;
-    max-height: 600px;
-    border: 1px solid white;
-    box-shadow: 0px 0px 6px white;
-  }
-
-  @media screen and (min-height: ${RESOLUTION_F_H}) and (max-width: ${RESOLUTION_F_W}) {
-    grid-auto-flow: row;
-    grid-template-columns: 1fr;
-    border-top-left-radius: 100px;
-    border-bottom-left-radius: 0;
-    border-top-right-radius: 100px;
-    border-bottom-right-radius: 0;
-  }
-`;
-
-export const Name = styled.span`
-  text-align: center;
-  font-size: 11vw;
-  font-weight: 800;
-  font-family: Geneva, Tahoma;
-  background: linear-gradient(#313c92 60%, #27c6c1);
-  -webkit-background-clip: text;
-  -webkit-text-fill-color: transparent;
-  border-bottom: 2px solid #00dfd8;
-  border-top: 2px solid #313c92;
-  transform: scaley(120%);
-  box-shadow: 0px 0px 6px white;
-  width: 100%;
-  padding: 0 10px;
-  align-self: end;
-  @media screen and (min-width: ${RESOLUTION_W}) {
-    font-size: 4.3em;
-  }
-  @media screen and (min-width: ${RESOLUTION_H}) {
-    width: auto;
-    margin: 20px 0 0 0;
-    align-self: center;
-  }
-`;
-
-export const BoxImgProfile = styled.div`
-  display: none;
-  img {
-    margin: auto;
-    border-radius: 555px;
-    border: 2px solid white;
-  }
-  @media screen and (min-width: ${RESOLUTION_H}) {
-    display: flex;
-    margin: auto;
-  }
-  @media screen and (min-height: ${RESOLUTION_F_H}) and (max-width: ${RESOLUTION_F_W}) {
-    display: flex;
-  }
+export const LinePower = styled.div`
+  position: absolute;
+  height: 100px;
+  top: 40px;
+  right: 0;
+  left: 0;
+  overflow: hidden;
 `;
