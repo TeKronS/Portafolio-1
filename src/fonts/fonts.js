526d3b206ec9d176664d94cf5be3b3d3aa29e1ed
TeKronS <asper.tk93@gmail.com>
2022-07-14 07:08:59 -0400

    added fonts

--- src/fonts/fonts.js
+++ src/fonts/fonts.js
@@ -1,8 +1,8 @@
 import { createGlobalStyle } from "styled-components";
 import Roboto1 from "./Roboto.woff";
 import Roboto2 from "./Roboto.woff2";
-import RobotoTop1 from "./ROBOTOP.woff";
-import RobotoTop2 from "./ROBOTOP.woff2";
+import Gotham1 from "./Gotham.woff";
+import Gotham2 from "./Gotham.woff2";
 
 export default createGlobalStyle`
     @font-face {
@@ -13,12 +13,13 @@ export default createGlobalStyle`
         font-weight: 500;
         font-style: normal;
     }
+   
     @font-face {
-        font-family: 'RobotoTopTK';
-        src: local('RobotoTopTK'), local('RobotoTopTK'),
-        url(${RobotoTop1}) format('woff'),
-        url(${RobotoTop2}) format('woff2');
-        font-weight: 300;
+        font-family: 'GothamTK';
+        src: local('GothamTK'), local('GothamTK'),
+        url(${Gotham1}) format('woff'),
+        url(${Gotham2}) format('woff2');
+        font-weight: 800;
         font-style: normal;
     }
 `;
